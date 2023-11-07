88b2ace5ca32e56c706c7ee8b76c62a799f0f5cb
nicolasjott <nicobox2002@gmail.com>
2023-11-06 23:31:36 -0500

    fix border color on focus

--- app/(auth)/login/index.tsx
+++ app/(auth)/login/index.tsx
@@ -11,48 +11,78 @@ import {
 } from "@gluestack-ui/themed";
 import { Link, Stack } from "expo-router";
 import { useState } from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
+
+import { useCallback } from "react";
 
 export default function Login() {
-  const [borderColor, setBorderColor] = useState("white");
+  const [borderColor, setBorderColor] = useState("#414040");
+  const onInputFocus = useCallback(() => {
+    setBorderColor("#EE0C0C");
+  }, []);
+
+  const onInputBlur = useCallback(() => {
+    setBorderColor("#414040");
+  }, []);
   return (
-    <Box flex={1} bg="#2F2E2E">
-      <Stack.Screen
-        options={{
-          title: "Log In",
-          headerShadowVisible: false,
-          headerStyle: { backgroundColor: "#2F2E2E" },
-          headerTintColor: "#EE0C0C",
-        }}
-      />
-      <Box flex={1} alignItems="center" justifyContent="center" m={8}>
-        <VStack w="90%" m="$16" space="3xl">
-          <FormControl>
-            <Input bg="#414040" borderColor="#2F2E2E">
-              <InputField color="white" type="text" placeholder="Email" />
-            </Input>
-          </FormControl>
-          <FormControl>
-            <Input bg="#414040" borderColor="#2F2E2E">
-              <InputField
-                color="white"
-                type="password"
-                placeholder="Password"
-              />
-            </Input>
-          </FormControl>
-          <HStack space="xs">
-            <Text color="white">New User?</Text>
-            <Link href={"/(auth)/register/"}>
-              <Text color="#EE0C0C" underline>
-                Register
-              </Text>
-            </Link>
-          </HStack>
-        </VStack>
-        <Button bgColor="#EE0C0C" px="$16">
-          <ButtonText>Log In</ButtonText>
-        </Button>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Box flex={1} bg="#2F2E2E">
+        <Stack.Screen
+          options={{
+            title: "Log In",
+            headerShadowVisible: false,
+            headerStyle: { backgroundColor: "#2F2E2E" },
+            headerTintColor: "#EE0C0C",
+          }}
+        />
+        <Box flex={1} alignItems="center" justifyContent="center" m={8}>
+          <VStack w="90%" m="$16" space="3xl">
+            <FormControl>
+              <Input
+                bg="#414040"
+                borderColor="#414040"
+                sx={{
+                  ":focus": {
+                    borderColor: "#EE0C0C",
+                  },
+                }}
+              >
+                <InputField color="white" type="text" placeholder="Email" />
+              </Input>
+            </FormControl>
+            <FormControl>
+              <Input
+                bg="#414040"
+                borderColor="#414040"
+                sx={{
+                  ":focus": {
+                    borderColor: "#EE0C0C",
+                  },
+                }}
+              >
+                <InputField
+                  color="white"
+                  type="password"
+                  placeholder="Password"
+                />
+              </Input>
+            </FormControl>
+            <HStack space="xs">
+              <Text color="white">New User?</Text>
+              <Link href={"/(auth)/register/"}>
+                <Text color="#EE0C0C" underline>
+                  Register
+                </Text>
+              </Link>
+            </HStack>
+          </VStack>
+          <Box>
+            <Button bgColor="#EE0C0C" px="$16">
+              <ButtonText>Log In</ButtonText>
+            </Button>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </TouchableWithoutFeedback>
   );
 }
--- app/(auth)/register/index.tsx
+++ app/(auth)/register/index.tsx
@@ -9,49 +9,76 @@ import {
 } from "@gluestack-ui/themed";
 import { Stack } from "expo-router";
 import { useState } from "react";
+import { Keyboard, TouchableWithoutFeedback } from "react-native";
 
 export default function Register() {
   const [borderColor, setBorderColor] = useState("white");
   return (
-    <Box flex={1} bg="#2F2E2E">
-      <Stack.Screen
-        options={{
-          title: "Register",
-          headerShadowVisible: false,
-          headerStyle: { backgroundColor: "#2F2E2E" },
-          headerTintColor: "#EE0C0C",
-        }}
-      />
-      <Box flex={1} alignItems="center" justifyContent="center" m={8}>
-        <VStack w="90%" m="$16" space="3xl">
-          <FormControl>
-            <Input bg="#414040" borderColor="#2F2E2E">
-              <InputField color="white" type="text" placeholder="Email" />
-            </Input>
-          </FormControl>
-          <FormControl>
-            <Input bg="#414040" borderColor="#2F2E2E">
-              <InputField
-                color="white"
-                type="password"
-                placeholder="Password"
-              />
-            </Input>
-          </FormControl>
-          <FormControl>
-            <Input bg="#414040" borderColor="#2F2E2E">
-              <InputField
-                color="white"
-                type="password"
-                placeholder="Confirm Password"
-              />
-            </Input>
-          </FormControl>
-        </VStack>
-        <Button bgColor="#EE0C0C" px="$16">
-          <ButtonText>Register</ButtonText>
-        </Button>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Box flex={1} bg="#2F2E2E">
+        <Stack.Screen
+          options={{
+            title: "Register",
+            headerShadowVisible: false,
+            headerStyle: { backgroundColor: "#2F2E2E" },
+            headerTintColor: "#EE0C0C",
+          }}
+        />
+        <Box flex={1} alignItems="center" justifyContent="center" m={8}>
+          <VStack w="90%" m="$16" space="3xl">
+            <FormControl>
+              <Input
+                bg="#414040"
+                borderColor="#2F2E2E"
+                sx={{
+                  ":focus": {
+                    borderColor: "#EE0C0C",
+                  },
+                }}
+              >
+                <InputField color="white" type="text" placeholder="Email" />
+              </Input>
+            </FormControl>
+            <FormControl>
+              <Input
+                bg="#414040"
+                borderColor="#2F2E2E"
+                sx={{
+                  ":focus": {
+                    borderColor: "#EE0C0C",
+                  },
+                }}
+              >
+                <InputField
+                  color="white"
+                  type="password"
+                  placeholder="Password"
+                />
+              </Input>
+            </FormControl>
+            <FormControl>
+              <Input
+                bg="#414040"
+                borderColor="#2F2E2E"
+                sx={{
+                  ":focus": {
+                    borderColor: "#EE0C0C",
+                  },
+                }}
+              >
+                <InputField
+                  color="white"
+                  type="password"
+                  placeholder="Confirm Password"
+                />
+              </Input>
+            </FormControl>
+          </VStack>
+          <Button bgColor="#EE0C0C" px="$16">
+            <ButtonText>Register</ButtonText>
+          </Button>
+        </Box>
       </Box>
-    </Box>
+    </TouchableWithoutFeedback>
   );
 }
