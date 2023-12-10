99f41cc2452c1d4e822c1de7391bea9fdbcae77b
nicolasjott <nicobox2002@gmail.com>
2023-12-09 19:42:52 -0500

    Add Spinner component and update ScrollView to Box in Roster component

--- app/(app)/rosters/[id].tsx
+++ app/(app)/rosters/[id].tsx
@@ -3,6 +3,7 @@ import {
   HStack,
   Heading,
   ScrollView,
+  Spinner,
   Text,
   VStack,
 } from "@gluestack-ui/themed";
@@ -35,7 +36,7 @@ export default function Roster() {
   });
 
   return (
-    <ScrollView flex={1} bg="#2F2E2E">
+    <Box flex={1} bg="#2F2E2E">
       <Stack.Screen
         options={{
           title: teamName,
@@ -50,64 +51,87 @@ export default function Roster() {
         }}
       />
       {roster && (
-        <Box flex={1} mb={insets.bottom}>
-          <Box bg="#EE0C0C" borderRadius={"$lg"} p={"$5"} mb={"$4"} m={"$2"}>
-            <VStack space="xl">
-              <Heading color="white">{roster.teamName}</Heading>
-              <HStack justifyContent="space-between">
-                <Text color="white">League Size: {roster.leagueSize}</Text>
-                <Text color="white">
-                  Draft Postition: {roster.draftPosition}
-                </Text>
-              </HStack>
-            </VStack>
+        <ScrollView flex={1} bg="#2F2E2E">
+          <Box flex={1} mb={insets.bottom}>
+            <Box bg="#EE0C0C" borderRadius={"$lg"} p={"$5"} mb={"$4"} m={"$2"}>
+              <VStack space="xl">
+                <Heading color="white">{roster.teamName}</Heading>
+                <HStack justifyContent="space-between">
+                  <Text color="white">League Size: {roster.leagueSize}</Text>
+                  <Text color="white">
+                    Draft Postition: {roster.draftPosition}
+                  </Text>
+                </HStack>
+              </VStack>
+            </Box>
+            <Heading color="white" m={"$4"}>
+              Draft Picks
+            </Heading>
+            <FlatList
+              scrollEnabled={false}
+              data={roster.players as Player[]}
+              keyExtractor={(item: Player, index: number) =>
+                `${index}_${item.name}`
+              }
+              renderItem={({
+                item,
+                index,
+              }: {
+                item: Player;
+                index: number;
+              }) => {
+                const { name, team, mainPos, sznAvgProj } = item;
+                return (
+                  <Box
+                    bgColor="#111111"
+                    py={"$4"}
+                    borderRadius={"$lg"}
+                    m={"$2"}
+                  >
+                    <HStack space="sm">
+                      <Box
+                        alignItems="center"
+                        justifyContent="center"
+                        w={"$16"}
+                        p={"$4"}
+                      >
+                        <Heading color="white">{index + 1}</Heading>
+                      </Box>
+                      <VStack space="xs" flex={1}>
+                        <Heading color="#EE0C0C">{name}</Heading>
+                        <HStack space="xs">
+                          <Text color="white">{team}</Text>
+                          <Text color="white">{mainPos}</Text>
+                        </HStack>
+                      </VStack>
+                      <VStack space="xs" flex={1}>
+                        <Heading color="white">Proj</Heading>
+                        <Text color="white">{sznAvgProj}</Text>
+                      </VStack>
+                    </HStack>
+                  </Box>
+                );
+              }}
+            />
+            <Heading color="white" alignSelf="flex-end" m={"$4"}>
+              Total Projected:{" "}
+              {roster.players
+                .reduce((total, player) => total + player.sznAvgProj, 0)
+                .toFixed(2)}
+            </Heading>
           </Box>
-          <Heading color="white" m={"$4"}>
-            Draft Picks
-          </Heading>
-          <FlatList
-            scrollEnabled={false}
-            data={roster.players as Player[]}
-            keyExtractor={(item: Player, index: number) =>
-              `${index}_${item.name}`
-            }
-            renderItem={({ item, index }: { item: Player; index: number }) => {
-              const { name, team, mainPos, sznAvgProj } = item;
-              return (
-                <Box bgColor="#111111" py={"$4"} borderRadius={"$lg"} m={"$2"}>
-                  <HStack space="sm">
-                    <Box
-                      alignItems="center"
-                      justifyContent="center"
-                      w={"$16"}
-                      p={"$4"}
-                    >
-                      <Heading color="white">{index + 1}</Heading>
-                    </Box>
-                    <VStack space="xs" flex={1}>
-                      <Heading color="#EE0C0C">{name}</Heading>
-                      <HStack space="xs">
-                        <Text color="white">{team}</Text>
-                        <Text color="white">{mainPos}</Text>
-                      </HStack>
-                    </VStack>
-                    <VStack space="xs" flex={1}>
-                      <Heading color="white">Proj</Heading>
-                      <Text color="white">{sznAvgProj}</Text>
-                    </VStack>
-                  </HStack>
-                </Box>
-              );
-            }}
-          />
-          <Heading color="white" alignSelf="flex-end" m={"$4"}>
-            Total Projected:{" "}
-            {roster.players
-              .reduce((total, player) => total + player.sznAvgProj, 0)
-              .toFixed(2)}
-          </Heading>
+        </ScrollView>
+      )}
+      {getRosterState.status === "loading" && (
+        <Box
+          flex={1}
+          alignItems="center"
+          justifyContent="center"
+          mb={insets.bottom}
+        >
+          <Spinner color={"#EE0C0C"} size={"large"} />
         </Box>
       )}
-    </ScrollView>
+    </Box>
   );
 }
